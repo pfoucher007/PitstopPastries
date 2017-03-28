35b02aa569edd3638044f6882e3d579794a4299c
nadamadkour <nada.e.madkour@gmail.com>
2017-03-28 08:14:09 -0400

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$db_host = 'localhost';
+$db_name = 'pPast';
+$db_user = 'root';
+$db_pass = 'root'; 
+
+$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+      echo"testting ";
+
+?>
